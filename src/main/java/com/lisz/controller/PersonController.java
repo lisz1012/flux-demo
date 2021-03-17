f42c023c1b2a23076fa0858c1609ea549f07df46
lisz1012 <lisz1012@163.com>
2021-03-17 01:12:16 -0700

    WebFlux实现SSE，服务器端不断推送数据

--- src/main/java/com/lisz/controller/PersonController.java
+++ src/main/java/com/lisz/controller/PersonController.java
@@ -3,13 +3,18 @@ package com.lisz.controller;
 import com.lisz.model.Person;
 import com.lisz.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.stream.IntStream;
 
 // 注解式、函数式 SpringCloud Gateway
 /*
@@ -48,7 +53,7 @@ public class PersonController {
 		// Return的时候 mono还没有被组装好，但是空的也可以，先return回容器里去，什么时候它有数据了就会吱一声，数据能用了再return到前端
 		// Netty容器阻塞了，也就是说在return之后还做了一些事情
 		// return 跟进去会发现进到了：org.springframework.web.reactive.result.method.InvocableHandlerMethod 调用到容器层了，知道是空值，但是没抛异常
-		// doOnSubscribe、doOnNext、sink.success、doOnSuccess根本不是在Controller里面做的，而是在Monoreturn回到Netty Web容器去之后被执行的
+		// doOnSubscribe、doOnNext、sink.success、doOnSuccess根本不是在Controller里面做的，而是在Mono return回到Netty Web容器去之后被执行的
 		// 感觉就是把Controller释放掉了，在Controller里面异步获取数据，上面的几个lambda表达式全部都是回调。
 		// 得到一个包装数据序列 ——> 包含特征 --> return给容器 --> 调用以上的回调
 		// 看起来像是异步，其实是在容器里阻塞了，因为其中有个Mono.just(...);还是阻塞的，只不过没发生在Controller曾，所以两次的System.out.println率先被打印了
@@ -66,7 +71,8 @@ public class PersonController {
 
 	@GetMapping("/aaa")
 	// ServerHttpRequest 是 webFlux中特有的，跟SpringMVC里的ServletRequest不一样
-	// 不一定用得到，但是拓展思维，了解现在比较前沿的技术框架是怎么构架出来的，自己是否可以写服务器？不可能，太难啦。生产环境就是SpringCloud Gateway，不会WebFlux的话看不太懂SpringCloud Gateway
+	// 不一定用得到，但是拓展思维，了解现在比较前沿的技术框架是怎么构架出来的，自己是否可以写服务器？不可能，太难啦。生产环境就是SpringCloud Gateway，不会WebFlux的话看不太懂SpringCloud Gateway（基本全是函数式）
+	// IO密集度比较高的时候，响应式web的性能就明显高了。底层是Netty全双工，长连接
 	public Mono<Object> get3(ServerHttpRequest request, String name, WebSession session){
 		System.out.println(request.getHeaders());
 		System.out.println("name: " + name); // 能得到name
@@ -86,5 +92,32 @@ public class PersonController {
 		}
 		System.out.println("Code = " + session.getAttribute("code"));
 		return Mono.just("haha");
+	}
+
+	@GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Person> sse(){
+		// 1. 封装对象
+		Flux<Person> flux = Flux.fromStream(IntStream.range(1, 10).mapToObj(i -> {
+			try {
+				Thread.sleep(new Random().nextInt(2000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return new Person(i, "xiaoming");
+		}))
+		.doOnSubscribe(sub -> {
+			System.out.println("订阅");
+		})
+		.doOnComplete(() -> { // 需要一个Runnable
+			System.out.println("Complete");
+		})
+		.doOnNext(data -> { // 有数据之后
+			System.out.println("有数据了：" + data);
+		});
+
+		// 2. 对象连带里面的方法给了容器. 返回的每个Person对象，做到了在前端即时显示，有一个Person被new出来了，就在前端显示一个，
+		//     客户端其实只发来了一次请求，全部Person对象返回之前，前端显示：Caution：请求还没结束。每个Person没重新包装成了个JSON（因为@RestController还在）
+		// 这还不是长连接，因为他还是基于http的，什么是长连接？TCP就是长连接，http就不是。这里类似于文件下载, 类比前面的SSE，但是这里没有Servlet的东西
+		return flux;
 	}
 }
